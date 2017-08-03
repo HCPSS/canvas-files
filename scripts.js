7d1294531b70b5acd421d164e7b18a5845735d6a
Audra Agnelly <aagnelly@yahoo.com>
2017-08-03 08:53:33 -0400

    Update GA; fix communities font
    Add User role metric to Google Analytics
    Add id label to Communities menu item

--- scripts.js
+++ scripts.js
@@ -31,7 +31,23 @@ $(document).ready(function(){
 	******************************************/
 	var sCourseName = null;
 	var sTemp;
+	//add userRole to analytics 8.3.17
+	var sUserRole
+	
+	//Get User Role
+    if ($.inArray('admin', ENV['current_user_roles']) == -1 && $.inArray('teacher', ENV['current_user_roles']) == -1 && $.inArray('student', ENV['current_user_roles']) > -1) {
+        sUserRole = "student"
+    } else if ($.inArray('admin', ENV['current_user_roles']) == -1 && $.inArray('teacher', ENV['current_user_roles']) > -1) {
+        sUserRole = "teacher"
+    } else if ($.inArray('admin', ENV['current_user_roles']) > -1) {
+        sUserRole = "admin"
+    } else {
+        sUserRole = "other"
+    }
+	console.log(sUserRole);
 
+    ga('set', 'dimension16', sUserRole); 
+	
 	try {
 		sTemp = window.location.pathname.match(/\/courses\/(\d+)/);
 		sCourseName = $("#section-tabs-header").text().trim().replace(/\n.*/, '').replace(/\s+/g, ' ').trim();
@@ -124,8 +140,9 @@ $(document).ready(function(){
 		Add extra links to dashboard
 	******************************************/
 	// Create container
-	$('#dashboard .col-xs-9').after('<ul id="extra-nav"></ul>');
-	$('.ic-Dashboard-header .grid-row').css('position', 'relative');
+	$('#dashboard .ic-Dashboard-header__title').after('<ul id="extra-nav"></ul>');
+	$('.ic-Dashboard-header').css('position', 'relative');
+	$('.ic-Dashboard-header__layout').css('padding-bottom','26px');
 
 	// Orientation link
 	var url = "https://hcpss.instructure.com/courses/9495";
@@ -176,13 +193,35 @@ $(document).ready(function(){
 		
 		// Accordion
 		$('.enhanceable_content.accordion div').hide();
+		$('.enhanceable_content.accordion table').hide();
+		
 		$('.enhanceable_content.accordion h3').click(function(){
 			$(this).next("div").slideToggle();
+			$(this).next("table").slideToggle();
 			return false;
 		});
 		
 		window.clearInterval(intervalID4);
 	}, 500);
+	
+	/*******************************************
+      Courses "Active Only" Toggle on Profiles
+	  /accounts/1/users/
+	*******************************************/
+	var courseListToggle = '<a class="courseListToggle btn" style="margin-bottom:10px;">Active Only</a>';
+	$(courseListToggle).insertBefore("#courses #courses_list");
+	
+	$(".courseListToggle").click(function(){
+		$("ul.context_list li.inactive, ul.context_list li.rejected, ul.context_list li.completed").slideToggle(function(){
+			if( $(this).is(":visible") ){
+				$(".courseListToggle").text("Active Only");
+			} else {
+				$(".courseListToggle").text("Show All");
+			}
+			
+			$("#courses_list h3").text('Courses ('+$("ul.context_list li:visible").size()+')');
+		});
+	});
 
 	/*******************************************
 		Remove 'report a problem' and 'submit a feature' from help dialog -
@@ -1858,3 +1897,4 @@ var msgobs = {
 $(document).ready(function () {
   msgobs.init();
 });
+
